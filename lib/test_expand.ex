34b6f160d7788abf55d420f8e0b4ed4e64d68436
Dunya Kirkali <dunyakirkali@gmail.com>
2020-06-24 20:35:12 +0200

    Initial commit

---
+++ lib/test_expand.ex
@@ -0,0 +1,18 @@
+defmodule TestExpand do
+  @moduledoc """
+  Documentation for `TestExpand`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestExpand.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/test_expand_test.exs
@@ -0,0 +1,8 @@
+defmodule TestExpandTest do
+  use ExUnit.Case
+  doctest TestExpand
+
+  test "greets the world" do
+    assert TestExpand.hello() == :world
+  end
+end
